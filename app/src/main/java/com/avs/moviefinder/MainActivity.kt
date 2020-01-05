17e478b9e1bac0052a11d6b8ef727fe765c77252
AlinaStepanova <stepanovaalina15@gmail.com>
2020-01-05 16:43:11 +0200

    change done icon

--- app/src/main/java/com/avs/moviefinder/MainActivity.kt
+++ app/src/main/java/com/avs/moviefinder/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.avs.moviefinder
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
