964d058b09e54e71c37c612bf6ac5eb5842063e7
AlinaStepanova <astepanova@yukon.cv.ua>
2020-05-31 20:04:22 +0300

    load image

--- app/src/main/java/com/avs/moviefinder/ui/find/FindAdapter.kt
+++ app/src/main/java/com/avs/moviefinder/ui/find/FindAdapter.kt
@@ -1,14 +1,26 @@
 package com.avs.moviefinder.ui.find
 
+import android.graphics.BitmapFactory
+import android.icu.lang.UCharacter.IndicPositionalCategory.LEFT
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.avs.moviefinder.network.dto.Movie
 import com.avs.moviefinder.R
+import com.avs.moviefinder.network.dto.Movie
+import com.avs.moviefinder.utils.POSTER_URL
+import com.avs.moviefinder.utils.POSTER_WIDTH
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropTransformation
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
+import java.net.URL
+
 
 class FindAdapter : ListAdapter<Movie, FindAdapter.ViewHolder>(MovieDiffCallback()) {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -20,14 +32,20 @@ class FindAdapter : ListAdapter<Movie, FindAdapter.ViewHolder>(MovieDiffCallback
         return ViewHolder.from(parent)
     }
 
-    class ViewHolder private constructor (itemView: View) : RecyclerView.ViewHolder(itemView){
-        private val title : TextView = itemView.findViewById(R.id.tvMovieTitle)
-        private val year : TextView = itemView.findViewById(R.id.tvMovieYear)
-        private val description : TextView = itemView.findViewById(R.id.tvMovieDescription)
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val title: TextView = itemView.findViewById(R.id.tvMovieTitle)
+        private val year: TextView = itemView.findViewById(R.id.tvMovieYear)
+        private val description: TextView = itemView.findViewById(R.id.tvMovieDescription)
+        private val poster: ImageView = itemView.findViewById(R.id.ivPoster)
         fun bind(item: Movie) {
             title.text = item.title
             year.text = item.year
             description.text = item.overview
+            Picasso.get()
+                .load(POSTER_URL + item.posterPath)
+                .transform(CropTransformation(0, 0, POSTER_WIDTH, POSTER_WIDTH))
+                .placeholder(R.drawable.ic_local_movies_grey)
+                .into(poster)
         }
 
         companion object {
@@ -44,7 +62,7 @@ class FindAdapter : ListAdapter<Movie, FindAdapter.ViewHolder>(MovieDiffCallback
 
 class MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
     override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
-        return oldItem.id== newItem.id
+        return oldItem.id == newItem.id
     }
 
     override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
--- app/src/main/java/com/avs/moviefinder/utils/Constants.kt
+++ app/src/main/java/com/avs/moviefinder/utils/Constants.kt
@@ -1,7 +1,8 @@
 package com.avs.moviefinder.utils
 
+const val POSTER_WIDTH = 1280
 const val BASE_URL = "https://api.themoviedb.org/3/movie/"
-const val POSTER_URL = "https://image.tmdb.org/t/p/w1280/"
+const val POSTER_URL = "https://image.tmdb.org/t/p/w$POSTER_WIDTH/"
 const val API_KEY = "3ae41cba5ffccb37830367a69287433d"
 //get single movie
 //https://api.themoviedb.org/3/movie/419704?api_key=3ae41cba5ffccb37830367a69287433d
