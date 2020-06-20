9758e51847cf58c9b363b3da84a4e19c2ecfabea
AlinaStepanova <astepanova@yukon.cv.ua>
2020-06-20 20:58:41 +0300

    add anchor to snack bar

--- app/src/main/java/com/avs/moviefinder/ui/BaseFragment.kt
+++ app/src/main/java/com/avs/moviefinder/ui/BaseFragment.kt
@@ -7,10 +7,12 @@ import com.google.android.material.snackbar.Snackbar
 open class BaseFragment : Fragment() {
 
     fun showSnackBar(message: String) {
+        val activity = (activity as MainActivity);
         val snackBar = Snackbar.make(
-            (activity as MainActivity).binding.navHostFragment, message,
+            activity.binding.navHostFragment, message,
             Snackbar.LENGTH_LONG
         )
+        snackBar.anchorView = activity.binding.navView
         snackBar.show()
     }
 }
