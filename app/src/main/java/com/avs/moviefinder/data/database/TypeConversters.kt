e27e6ec307b45aa0221dba6cc59ac8c6e1b658b0
AlinaStepanova <astepanova@yukon.cv.ua>
2021-01-01 17:34:10 +0200

    update file structure for the type converters

--- app/src/main/java/com/avs/moviefinder/data/database/TypeConversters.kt
+++ app/src/main/java/com/avs/moviefinder/data/database/TypeConversters.kt
@@ -1,7 +1,23 @@
 package com.avs.moviefinder.data.database
 
 import androidx.room.TypeConverter
+import com.avs.moviefinder.data.dto.Country
 import com.avs.moviefinder.data.dto.Genre
+
+open class CountriesConverter {
+
+    @TypeConverter
+    open fun fromCountries(countries: List<Country>): String {
+        return countries.fold("") { names, country ->
+            if (names.isEmpty()) country.name ?: "" else names + "," + country.name
+        }
+    }
+
+    @TypeConverter
+    open fun toCountries(countries: String): List<Country> {
+        return countries.split(",").map { Country(it.trim()) }
+    }
+}
 
 open class GenresConverter {
 
