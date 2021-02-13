2f7878083b025e9c10f13e944d3924d1b2ab6e62
AlinaStepanova <astepanova@yukon.cv.ua>
2021-02-13 19:15:21 +0200

    implement movie repository

---
+++ app/src/main/java/com/avs/moviefinder/repository/MovieRepository.kt
@@ -0,0 +1,59 @@
+package com.avs.moviefinder.repository
+
+import com.avs.moviefinder.data.database.DatabaseManager
+import com.avs.moviefinder.data.dto.Movie
+import com.avs.moviefinder.data.network.ServerApi
+import com.avs.moviefinder.utils.RxBus
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MovieRepository @Inject constructor(
+    private val serverApi: ServerApi,
+    private val databaseManager: DatabaseManager,
+    private val rxBus: RxBus
+) {
+    private val compositeDisposable = CompositeDisposable()
+    private var extrasMovie = Movie()
+
+    private fun combineTwoMovies(apiMovie: Movie?, dbMovie: Movie?) {
+        if (dbMovie != null) {
+            extrasMovie = dbMovie
+        }
+        apiMovie?.let {
+            if (!it.imdbId.isNullOrEmpty()) extrasMovie.imdbId = it.imdbId
+            if (!it.homepage.isNullOrEmpty()) extrasMovie.homepage = it.homepage
+            if (!it.genres.isNullOrEmpty()) extrasMovie.genres = it.genres
+            if (!it.tagline.isNullOrEmpty()) extrasMovie.tagline = it.tagline
+            if (it.runtime != 0) extrasMovie.runtime = it.runtime
+        }
+    }
+
+    fun getMovieData(movie: Movie) {
+        extrasMovie = movie
+        compositeDisposable.add(Single.zip(
+            serverApi.callMovieById(movie.id)
+                .onErrorReturn { extrasMovie },
+            databaseManager.getMovieByIdAsSingle(movie.id)
+                .onErrorReturn { extrasMovie }
+        ) { apiMovie, dbMovie -> combineTwoMovies(apiMovie, dbMovie) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                if (extrasMovie.isFavorite || extrasMovie.isInWatchLater) {
+                    insertMovie(extrasMovie)
+                } else {
+                    rxBus.send(extrasMovie)
+                }
+            }, {})
+        )
+    }
+
+    fun insertMovie(movie: Movie) = compositeDisposable.add(databaseManager.insertMovie(movie))
+
+    fun clear() = compositeDisposable.clear()
+}
--- app/src/main/java/com/avs/moviefinder/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/avs/moviefinder/ui/movie/MovieViewModel.kt
@@ -1,24 +1,17 @@
 package com.avs.moviefinder.ui.movie
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.avs.moviefinder.data.database.DatabaseManager
 import com.avs.moviefinder.data.dto.Movie
-import com.avs.moviefinder.data.network.ServerApi
+import com.avs.moviefinder.repository.MovieRepository
 import com.avs.moviefinder.utils.RxBus
 import com.avs.moviefinder.utils.buildShareLink
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class MovieViewModel @Inject constructor(
-    private val serverApi: ServerApi,
-    private val databaseManager: DatabaseManager,
+    private val movieRepository: MovieRepository,
     rxBus: RxBus
 ) : ViewModel() {
     private var _movie = MutableLiveData<Movie?>()
@@ -27,68 +20,33 @@ class MovieViewModel @Inject constructor(
     private var _shareBody = MutableLiveData<String?>()
     val shareBody: LiveData<String?>
         get() = _shareBody
-    private var rxBusDisposable: Disposable? = null
-    private var apiDisposable: Disposable? = null
-    private var extrasMovie = Movie()
+    private var rxBusDisposable: Disposable
     private var isInitiallyFavorite = false
     private var isInitiallyInWatchList = false
-    private val compositeDisposable = CompositeDisposable()
 
     init {
-        rxBusDisposable = rxBus.events.subscribe { event -> handleServerResponse(event) }
+        rxBusDisposable = rxBus.events.subscribe { event -> subscribeToEvents(event) }
     }
 
     override fun onCleared() {
-        apiDisposable?.dispose()
-        rxBusDisposable?.dispose()
+        rxBusDisposable.dispose()
+        movieRepository.clear()
         super.onCleared()
     }
 
-    private fun handleServerResponse(event: Any?) {
+    private fun subscribeToEvents(event: Any?) {
         if (event is Movie) {
             _movie.value = event
-        } else if (event is Throwable) {
-            _movie.value = extrasMovie
-            Log.d(this::class.java.name, event.toString())
-        }
-    }
-
-    private fun combineTwoMovies(apiMovie: Movie?, dbMovie: Movie?) {
-        apiMovie?.let {
-            if (!it.imdbId.isNullOrEmpty()) extrasMovie.imdbId = it.imdbId
-            if (!it.homepage.isNullOrEmpty()) extrasMovie.homepage = it.homepage
-            if (!it.genres.isNullOrEmpty()) extrasMovie.genres = it.genres
-            if (!it.tagline.isNullOrEmpty()) extrasMovie.tagline = it.tagline
-            if (it.runtime != 0) extrasMovie.runtime = it.runtime
-        }
-        dbMovie?.let {
-            extrasMovie.isInWatchLater = it.isInWatchLater
-            extrasMovie.isFavorite = it.isFavorite
         }
     }
 
     fun openMovieDetails(movie: Movie?) {
-        if (movie != null) {
-            _movie.value?.posterPath = movie.posterPath
-            _movie.value?.title = movie.title
-            extrasMovie = movie
-            isInitiallyFavorite = movie.isFavorite
-            isInitiallyInWatchList = movie.isInWatchLater
-            apiDisposable?.dispose()
-            apiDisposable = Single.zip(
-                serverApi.callMovieById(movie.id).onErrorReturn { extrasMovie },
-                databaseManager.getMovieByIdAsSingle(movie.id)
-                    .doOnError { compositeDisposable.add(serverApi.getMovieById(movie.id)) }
-            ) { apiMovie, dbMovie -> combineTwoMovies(apiMovie, dbMovie) }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    if (extrasMovie.isFavorite || extrasMovie.isInWatchLater) {
-                        compositeDisposable.add(databaseManager.update(extrasMovie))
-                    } else {
-                        _movie.value = extrasMovie
-                    }
-                }, {})
+        movie?.let {
+            _movie.value?.posterPath = it.posterPath
+            _movie.value?.title = it.title
+            isInitiallyFavorite = it.isFavorite
+            isInitiallyInWatchList = it.isInWatchLater
+            movieRepository.getMovieData(it)
         }
     }
 
@@ -99,7 +57,7 @@ class MovieViewModel @Inject constructor(
             if (isInWatchLater) {
                 it.lastTimeUpdated = System.currentTimeMillis()
             }
-            compositeDisposable.add(databaseManager.insertMovie(it))
+            movieRepository.insertMovie(it)
         }
     }
 
@@ -110,7 +68,7 @@ class MovieViewModel @Inject constructor(
             if (isFavorite) {
                 it.lastTimeUpdated = System.currentTimeMillis()
             }
-            compositeDisposable.add(databaseManager.insertMovie(it))
+            movieRepository.insertMovie(it)
         }
     }
 
